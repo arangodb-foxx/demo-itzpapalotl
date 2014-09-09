631d5772f55fff010fbf7f06c5ddff4b0e941ace
jsteemann <j.steemann@triagens.de>
2014-09-09 23:24:31 +0200

    built version 0.0.5

--- README.md
+++ README.md
@@ -2,9 +2,10 @@ itzpapalotl
 ===========
 
 This ia a simple example application written for ArangoDB-Foxx.
-It returns a random Aztec god name.
-
+It returns a random Aztec deity name.
 
 The application provides a simple REST API with the following methods:
-* `GET /itzpapalotl/random`: return a random god name
+* `GET /itzpapalotl/index`: shows an HTML overview page
+* `GET /itzpapalotl/random`: returns a random deity name as JSON
+* `GET /itzpapalotl/{deity}/summon: summon a deity
 
--- itzpapalotl.js
+++ itzpapalotl.js
@@ -31,45 +31,103 @@
 (function() {
   "use strict";
 
-  // Initialise a new FoxxApplication.
-  var FoxxApplication = require("org/arangodb/foxx").Controller,
-  controller = new FoxxApplication(applicationContext);
+  // initialise a new FoxxApplication
+  var FoxxApplication = require("org/arangodb/foxx").Controller;
+  var controller = new FoxxApplication(applicationContext);
 
-  // install route
+  // include console module so we can log something (in the server's log)
+  var console = require("console");
+  var ArangoError = require("org/arangodb").ArangoError;
+
+  // we also need this module for custom responses
+  var actions = require("org/arangodb/actions");
+
+  // our app is about the following Aztec gods:
+  var deities = [
+    "CentzonTotochtin",
+    "Chalchihuitlicue",
+    "Chantico",
+    "Chicomecoatl",
+    "Cihuacoatl",
+    "Cinteotl",
+    "Coatlicue",
+    "Coyolxauhqui",
+    "Huehuecoyotl",
+    "Huitzilopochtli",
+    "Ilamatecuhtli",
+    "Itzcoliuhqui",
+    "Itzpaplotl",
+    "Mayauel",
+    "Meztli",
+    "Mictlantecuhtli",
+    "Mixcoatl",
+    "Quetzalcoatl",
+    "Tezcatlipoca",
+    "Tialoc",
+    "Tlauixcalpantecuhtli",
+    "Tlazolteotl",
+    "Tonatiuh",
+    "Tozi",
+    "XipeTotec",
+    "Xochiquetzal",
+    "Xolotl",
+    "Yacatecuhtli"
+  ];
+
+  // install index route (this is the default route mentioned in manifest.json)
+  // this route will create an HTML overview page
+  controller.get('/index', function (req, res) {
+    res.contentType = "text/html";
+
+    var body = "<h1>" + applicationContext.name + " (" + applicationContext.version + ")</h1>";
+    body += "<h2>an example application demoing a few Foxx features</h2>";
+
+    deities.forEach(function(deity) {
+      body += "summon <a href=\"" + encodeURIComponent(deity) + "/summon\">" + deity + "</a><br />";
+    });
+
+    body += "<hr />pick a <a href=\"random\">random</a> Aztec deity";
+
+    res.body = body;
+  })
+  .summary("prints an overview page");
+
+  // install route to return a random deity name in JSON
   controller.get('/random', function (req, res) {
-    var gods = [
-      "CentzonTotochtin",
-      "Chalchihuitlicue",
-      "Chantico",
-      "Chicomecoatl",
-      "Cihuacoatl",
-      "Cinteotl",
-      "Coatlicue",
-      "Coyolxauhqui",
-      "Huehuecoyotl",
-      "Huitzilopochtli",
-      "Ilamatecuhtli",
-      "Itzcoliuhqui",
-      "Itzpaplotl",
-      "Mayauel",
-      "Meztli",
-      "Mictlantecuhtli",
-      "Mixcoatl",
-      "Quetzalcoatl",
-      "Tezcatlipoca",
-      "Tialoc",
-      "Tlauixcalpantecuhtli",
-      "Tlazolteotl",
-      "Tonatiuh",
-      "Tozi",
-      "XipeTotec",
-      "Xochiquetzal",
-      "Xolotl",
-      "Yacatecuhtli"
-    ];
+    var idx = Math.round(Math.random() * (deities.length - 1));
+    res.json({ name: deities[idx] });
+  })
+  .summary("returns a random deity name");
+  
+  // install god-specific route for summoning
+  // god name is passed as part of the URL
+  controller.get('/:deity/summon', function (req, res) {
+    var deity = req.params("deity");
 
-    var idx = Math.round(Math.random() * (gods.length - 1));
-    res.json({ name: gods[idx] });
-  });
+    console.log("request to summon %s", deity);
+
+    if (deities.indexOf(deity) === -1) {
+      // unknown deity
+      throw new ArangoError();
+    }
+
+    console.log("summoning %s", deity);
+    
+    res.json({ name: deity, summoned: true });
+  })
+  .summary("summons the requested deity")
+  .pathParam("deity", {
+    type: "string"
+  })
+  .errorResponse(
+    Error, actions.HTTP_NOT_FOUND, "The requested deity could not be found", function(e) {
+      return {
+        error: true,
+        code: actions.HTTP_NOT_FOUND,
+        errorMessage: "The requested deity could not be found"
+      };
+    }
+  );
+
 }());
 
